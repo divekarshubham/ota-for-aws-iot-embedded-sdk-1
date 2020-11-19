1923b86b1aa3e44aed36e03718f7eb8febe8fab2
Tiangang Song <ts.whu@outlook.com>
2020-11-18 20:11:53 -0800

    Fix a pointer ops in job parsing

--- source/ota.c
+++ source/ota.c
@@ -613,6 +613,7 @@ static void setPALCallbacks( const OtaPalCallbacks_t * pCallbacks )
 {
     /* Initialize ota pal callback.*/
     OtaPalCallbacks_t palCallbacks = OTA_JOB_CALLBACK_DEFAULT_INITIALIZER;
+
     otaAgent.palCallbacks = palCallbacks;
 
     if( pCallbacks->abortUpdate != NULL )
@@ -1464,7 +1465,7 @@ static DocParseErr_t extractParameter( JsonDocParam_t docParam,
     pParamAdd = ( uint8_t * ) pContextBase + docParam.pDestOffset;
 
     /* Get destination buffer size to parameter storage location. */
-    pParamSizeAdd = pContextBase + docParam.pDestSizeOffset;
+    pParamSizeAdd = ( uint32_t * ) ( ( uint8_t * ) pContextBase + docParam.pDestSizeOffset );
 
     if( ( ModelParamTypeStringCopy == docParam.modelParamType ) || ( ModelParamTypeArrayCopy == docParam.modelParamType ) )
     {
